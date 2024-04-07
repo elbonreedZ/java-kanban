a0a94443a985a894c20e607d52fc59ecb4d839da
Елена <elbonreed05@yandex.ru>
2024-04-07 16:34:12 +0300

    checkStyle in Managers

--- src/main/java/ru/yandex/javacourse/russkina/schedule/manager/Managers.java
+++ src/main/java/ru/yandex/javacourse/russkina/schedule/manager/Managers.java
@@ -1,15 +1,15 @@
 package ru.yandex.javacourse.russkina.schedule.manager;
 
 public class Managers {
-    public static TaskManager getDefault(){
+    public static TaskManager getDefault() {
         return getInMemoryTaskManager();
     }
 
-    public static InMemoryTaskManager getInMemoryTaskManager(){
+    public static InMemoryTaskManager getInMemoryTaskManager() {
         return new InMemoryTaskManager();
     }
 
-    public static HistoryManager getDefaultHistory(){
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 }
