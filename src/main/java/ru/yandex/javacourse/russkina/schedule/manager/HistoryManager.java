6e4f8b3ee3ab21846f99884c8d2a3aaaa2de7846
Елена <elbonreed05@yandex.ru>
2024-04-07 16:13:30 +0300

    checkStyle in HistoryManager

--- src/main/java/ru/yandex/javacourse/russkina/schedule/manager/HistoryManager.java
+++ src/main/java/ru/yandex/javacourse/russkina/schedule/manager/HistoryManager.java
@@ -2,12 +2,13 @@ package ru.yandex.javacourse.russkina.schedule.manager;
 
 import ru.yandex.javacourse.russkina.schedule.task.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
 
 }
